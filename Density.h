95ed4058db3f532172ee492a4d202b0b799aa9b6
James Clough <jclough@ieee.org>
2004-11-14 21:49:16 +1100

    WD 0.1a1

--- Density.h
+++ Density.h
@@ -7,18 +7,21 @@
 {
 	NSString *header;
     NSString *outFile;
+    int maxjb;
     calc_params params;
 }
+
+- (int)nvals;
 
 - (void)calculate;
 
 /**
  * Parse input with the observation data separated by spaces
-**/
+ **/
 - (BOOL)parseInputSpaces:(NSString *)input;
 
 /**
  * Parse input with the observation data separated by commas
-**/
+ **/
 - (BOOL)parseInputCommas:(NSString *)input;
 @end
