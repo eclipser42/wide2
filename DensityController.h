529c7bf28112a3a8c48649da4ef92a6574253fa2
James Clough <jclough@ieee.org>
2005-06-03 01:17:18 +1000

    WD 0.1a16 - Add progress bar to calculation tab (take 1, abusing MVC pattern)

--- DensityController.h
+++ DensityController.h
@@ -6,6 +6,11 @@
 @interface DensityController : NSObject
 {
     IBOutlet Density *document;
+    IBOutlet NSButton *calculateButton;
+    IBOutlet NSProgressIndicator *progressBar;
+    NSTimer *progressTimer;
 }
 - (IBAction)calculate:(id)sender;
+- (void)updateProgress:(id)ignored;
+- (void)finishCalculation;
 @end
