8e9328cb2bc3742b626c1a649373ce22e8cc5cdb
James Clough <jclough@ieee.org>
2006-05-06 20:48:05 +1000

    WD 0.2a6 - First full UI - Add icon for WDdata documents - Remove IMV from input, compute it

--- DensityController.h
+++ DensityController.h
@@ -8,7 +8,17 @@
     IBOutlet Density *document;
     IBOutlet NSButton *calculateButton;
     IBOutlet NSProgressIndicator *progressBar;
+    NSString *detectionUnitString;
+    NSString *estdenUnitString;
 }
+- (int)censusType;
+- (void)setCensusType:(int)censusType;
+- (BOOL)distancesSuppliedEnabled;
+- (int)transectUnit;
+- (void)setTransectUnit:(int)transectUnit;
+- (BOOL)detectionUnitEnabled;
+- (int)detectionUnit;
+- (void)setDetectionUnit:(int)detectionUnit;
 - (IBAction)calculate:(id)sender;
 - (void)updateProgress:(NSTimer*)timer;
 @end
