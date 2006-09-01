43b8d48c16b9badffc4c4b61ece71d6b6e8ee0bb
James Clough <jclough@ieee.org>
2006-09-02 08:43:51 +1000

    WD0.2a8: - Calculate in main thread on Panther, separate thread on Tiger or later

--- DensityController.h
+++ DensityController.h
@@ -10,10 +10,17 @@
     IBOutlet NSProgressIndicator *progressBar;
     NSString *detectionUnitString;
     NSString *estdenUnitString;
+    int maximumClassDistance;
 }
 - (int)censusType;
 - (void)setCensusType:(int)censusType;
 - (BOOL)distancesSuppliedEnabled;
+- (int)observationType;                         // KDT
+- (void)setObservationType:(int)observationType;
+- (BOOL)classDistancesEnabled;
+//- (int)minimumClassDistance;                    // STT
+//- (void)setClassMinimum:(int)minimumClassDistance;
+- (void)setMaximumClassDistance:(int)maximumClassDistance;
 - (int)transectUnit;
 - (void)setTransectUnit:(int)transectUnit;
 - (BOOL)detectionUnitEnabled;
